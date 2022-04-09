d0cc612e3b8747e5df9a44762ce47d2fb65022ee
Martin Thoma <info@martin-thoma.de>
2022-04-09 23:08:40 +0200

    Initial commit adding CI, pre-commit + 2-up.py

---
+++ 2-up.py
@@ -0,0 +1,35 @@
+"""
+Create a booklet-style PDF from a single input.
+
+Pairs of two pages will be put on one page (left and right)
+
+usage: python 2-up.py input_file output_file
+"""
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import sys
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("usage: python 2-up.py input_file output_file")
+        sys.exit(1)
+    print("2-up input " + sys.argv[1])
+    reader = PdfFileReader(open(sys.argv[1], "rb"))
+    writer = PdfFileWriter()
+    for iter in range(0, reader.getNumPages() - 1, 2):
+        lhs = reader.getPage(iter)
+        rhs = reader.getPage(iter + 1)
+        lhs.mergeTranslatedPage(rhs, lhs.mediaBox.getUpperRight_x(), 0, True)
+        writer.addPage(lhs)
+        print(str(iter) + " "),
+        sys.stdout.flush()
+
+    print(f"writing {sys.argv[2]}")
+    with open(sys.argv[2], "wb") as fp:
+        writer.write(fp)
+    print("done.")
+
+
+if __name__ == "__main__":
+    main()
