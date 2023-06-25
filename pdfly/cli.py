fcc68547e9285d494be8a973768cc438db3094c0
Martin Thoma <info@martin-thoma.de>
2023-06-25 09:58:10 +0200

    MAINT: Enhance validation of 'pdf' arguments

--- pdfly/cli.py
+++ pdfly/cli.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from typing import List
 
 import typer
+from typing_extensions import Annotated
 
 import pdfly.cat
 import pdfly.compress
@@ -18,8 +19,11 @@ import pdfly.up2
 
 
 def version_callback(value: bool) -> None:
+    import pypdf
+
     if value:
         typer.echo(f"pdfly {pdfly.__version__}")
+        typer.echo(f"  using pypdf=={pypdf.__version__}")
         raise typer.Exit
 
 
@@ -42,12 +46,37 @@ def common(
 
 
 @entry_point.command(name="extract-images")  # type: ignore[misc]
-def extract_images(pdf: Path) -> None:
+def extract_images(
+    pdf: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ]
+) -> None:
     pdfly.extract_images.main(pdf)
 
 
 @entry_point.command(name="2-up")  # type: ignore[misc]
-def up2(pdf: Path, out: Path) -> None:
+def up2(
+    pdf: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ],
+    out: Path,
+) -> None:
     pdfly.up2.main(pdf, out)
 
 
@@ -67,7 +96,17 @@ def cat(
 
 @entry_point.command(name="meta")  # type: ignore[misc]
 def metadata(
-    pdf: Path,
+    pdf: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ],
     output: pdfly.metadata.OutputOptions = typer.Option(  # noqa
         pdfly.metadata.OutputOptions.text.value,
         "--output",
@@ -81,7 +120,17 @@ def metadata(
 
 @entry_point.command(name="pagemeta")  # type: ignore[misc]
 def pagemeta(
-    pdf: Path,
+    pdf: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ],
     page_index: int,
     output: pdfly.metadata.OutputOptions = typer.Option(  # noqa
         pdfly.metadata.OutputOptions.text.value,
@@ -99,7 +148,19 @@ def pagemeta(
 
 
 @entry_point.command(name="extract-text")  # type: ignore[misc]
-def extract_text(pdf: Path) -> None:
+def extract_text(
+    pdf: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ]
+) -> None:
     """Extract text from a PDF file."""
     from pypdf import PdfReader
 
@@ -109,7 +170,20 @@ def extract_text(pdf: Path) -> None:
 
 
 @entry_point.command(name="compress")  # type: ignore[misc]
-def compress(pdf: Path, output: Path) -> None:
+def compress(
+    pdf: Annotated[
+        Path,
+        typer.Argument(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ],
+    output: Path,
+) -> None:
     pdfly.compress.main(pdf, output)
 
 
