542c1cfda3b94b310029a961cb414867fd03eaba
kommade <khoojarrell@gmail.com>
2023-10-14 23:52:49 +0800

    Final commit

--- pdfly/cat.py
+++ pdfly/cat.py
@@ -64,7 +64,6 @@ def main(
         sys.stdout.flush()
         output_fh = os.fdopen(sys.stdout.fileno(), "wb")
 
-    #merger = PdfMerger()
     writer = PdfWriter()
     in_fs = {}
     try:
@@ -73,11 +72,10 @@ def main(
                 print(filename, page_range, file=sys.stderr)
             if filename not in in_fs:
                 in_fs[filename] = open(filename, "rb")
-            #merger.append(in_fs[filename], pages=page_range)
+
             reader = PdfReader(filename)
             for page_num in range(*page_range.indices(len(reader.pages))):
                 writer.add_page(reader.pages[page_num - 1])
-        #merger.write(output_fh)
         writer.write(output_fh)
     except Exception:
         print(traceback.format_exc(), file=sys.stderr)
