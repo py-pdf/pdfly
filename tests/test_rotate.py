cda2503c842e065bec6286f336012ce991e92181
Hal Wine <hal@dtor.com>
2025-05-01 08:52:39 -0700

    DEV: add additional test coverage
    Test coverage now all but execption handling in pdfly/rotate.py

--- tests/test_rotate.py
+++ tests/test_rotate.py
@@ -43,7 +43,9 @@ def get_page_rotations(fname: Path) -> list[int]:
     return rotations
 
 
-def diff_rotations(degrees: int, in_: list[int], out: list[int]) -> list[int]:
+def diff_rotations(
+    in_: list[int], out: list[int], degrees: int = 0
+) -> list[int]:
     diffs = []
     for orig, rotated in zip(in_, out):
         diffs.append(rotated - (orig + degrees))
@@ -71,79 +73,37 @@ def test_rotate_default(capsys, tmp_path):
 
     assert exit_code == 0
 
-    assert not any(diff_rotations(degrees, in_rotations, out_rotations))
-    # can test rotation values
+    assert not any(diff_rotations(in_rotations, out_rotations, degrees))
 
 
-# ToDo: needs major restructuring, but want to leave syntax framework
-# for now
+@pytest.mark.parametrize(
+    # NB "slice" can not be specified as the empty string
+    ("degrees", "slice", "expected_diff"),
+    [
+        (90, ":", [90, 90, 90, 90, 90, 90, 90, 90]),  # every page
+        (90, "::2", [90, 0, 90, 0, 90, 0, 90, 0]),  # every other, even index
+        (90, "1::2", [0, 90, 0, 90, 0, 90, 0, 90]),  # every other, odd index
+        (90, ":2", [90, 90, 0, 0, 0, 0, 0, 0]),  # first 2
+    ],
+)
+def test_rotate_slices(capsys, tmp_path, degrees, slice, expected_diff):
+    in_fname = str(RESOURCES_ROOT / "input8.pdf")
+    out_fname = "output.pdf"
+    with chdir(tmp_path):
+        args = [
+            "rotate",
+            "-o",
+            f"{out_fname}",
+            f"{in_fname}",
+            f"{degrees}",
+            f"{slice}",
+        ]
+        exit_code = run_cli(args)
+        captured = capsys.readouterr()
+        assert exit_code == 0, captured.err
 
-## @pytest.mark.parametrize(
-##     ("page_count", "expected", "expected_bc"),
-##     [
-##         ("8", "81\n27\n63\n45\n", "81\n27\n63\n45\n"),
-##         ("7", "71\n2\n63\n45\n", "71\n2b\n63\n45\n"),
-##         ("6", "61\n25\n43\n\n", "61\n25\n43\nc\n"),
-##         ("5", "51\n2\n43\n\n", "51\n2b\n43\nc\n"),
-##         ("4", "41\n23\n", "41\n23\n"),
-##         ("3", "31\n2\n", "31\n2b\n"),
-##         ("2", "21\n\n", "21\nc\n"),
-##         ("1", "1\n\n", "1b\nc\n"),
-##     ],
-## )
-## def test_rotate_order(capsys, tmp_path, page_count, expected, expected_bc):
-##     with chdir(tmp_path):
-##         exit_code = run_cli(
-##             [
-##                 "cat",
-##                 "-o",
-##                 f"input{page_count}.pdf",
-##                 str(RESOURCES_ROOT / "input8.pdf"),
-##                 f":{page_count}",
-##             ]
-##         )
-##         assert exit_code == 0
-##
-##         exit_code = run_cli(
-##             [
-##                 "rotate",
-##                 f"input{page_count}.pdf",
-##                 f"output{page_count}.pdf",
-##             ]
-##         )
-##         captured = capsys.readouterr()
-##         assert exit_code == 0, captured.err
-##
-##         exit_code = run_cli(
-##             [
-##                 "extract-text",
-##                 f"output{page_count}.pdf",
-##             ]
-##         )
-##         captured = capsys.readouterr()
-##         assert exit_code == 0, captured.err
-##         assert captured.out == expected
-##
-##         exit_code = run_cli(
-##             [
-##                 "rotate",
-##                 "--centerfold-file",
-##                 str(RESOURCES_ROOT / "c.pdf"),
-##                 "--blank-page-file",
-##                 str(RESOURCES_ROOT / "b.pdf"),
-##                 f"input{page_count}.pdf",
-##                 f"outputbc{page_count}.pdf",
-##             ]
-##         )
-##         captured = capsys.readouterr()
-##         assert exit_code == 0, captured.err
-##
-##         exit_code = run_cli(
-##             [
-##                 "extract-text",
-##                 f"outputbc{page_count}.pdf",
-##             ]
-##         )
-##         captured = capsys.readouterr()
-##         assert exit_code == 0, captured.err
-##         assert captured.out == expected_bc
+        in_rotations = get_page_rotations(in_fname)
+        out_rotations = get_page_rotations(out_fname)
+        actual_diff = diff_rotations(in_rotations, out_rotations)
+
+    assert not any(diff_rotations(actual_diff, expected_diff))
